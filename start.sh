e866e239f5c302ddaa29c8b34f914c7a7b30f039
I333730 <denis.duev@sap.com>
2020-05-24 17:37:29 +0300

    add actions

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 8081:8081 denisduev/covid-19:latest
+docker run -d -p 8081:8081  --name covid_19_news_rest denisduev/covid-19:latest
---
+++ stop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker stop covid_19_news_rest
