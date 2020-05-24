c0d7cdeb7e7fefe58baee1333b06ff74ffff6eba
I333730 <denis.duev@sap.com>
2020-05-24 18:02:57 +0300

    add frontend container

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-docker run -d -p 8081:8081  --name covid_19_news_rest denisduev/covid-19:latest
+echo "Starting docker containers needed for covid-19 app..."
+
+echo "Starting backend - spring boot REST API"
+docker run -d -p 8081:8081 --rm --name covid_19_news_rest denisduev/covid-19:latest
+
+echo "Starting frontend - vue.js application"
+docker run -d -p 8080:8080 --rm --name covid_19_vuejs denisduev/covid-19-vuejs:latest
+
+echo "You can access the app on: http://localhost:8080"
--- stop.sh
+++ stop.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+echo "Stopping docker containers needed for covid-19 app..."
+
+echo "Stopping backend - spring boot REST API"
 docker stop covid_19_news_rest
+
+echo "Stopping frontend - vue.js application"
+docker stop covid_19_vuejs
+
+echo "Container should be stopped and deleted :)"
