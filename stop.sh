a8f93383d3c63e5714e3621a88c2c0780e167aae
Denis Duev <denis_duev@abv.bg>
2020-05-26 11:32:03 +0300

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -5,6 +5,9 @@ echo "Stopping docker containers needed for covid-19 app..."
 echo "Stopping backend - spring boot REST API"
 docker stop covid_19_news_rest
 
+echo "Stopping backend - .NET app, websockets"
+docker stop covid19_asp_net_core
+
 echo "Stopping frontend - vue.js application"
 docker stop covid_19_vuejs
 
